7646a80979af23fd3ac506f3042503b1ca6207da
MiguelCordeiro16 <miguelcordeirosilva08@gmail.com>
2024-06-06 22:22:08 -0300

    tela de listagem css e da tela CardDoação

--- DoaMaisMobile/src/Pages/Listagem/Listagem.jsx
+++ DoaMaisMobile/src/Pages/Listagem/Listagem.jsx
@@ -3,110 +3,82 @@ import {
   Text,
   View,
   StyleSheet,
-  Image,
-  TouchableOpacity,
+  ScrollView,
+  TextInput,
 } from 'react-native';
-import ApiService from './ApiService'; // Ajuste o caminho conforme necessário
+import ApiService from '../../Services/ApiService';
+import CardDoacao from '../../Components/CardDoacao';
 
 export default function Listagem() {
-  const [imageUrl, setImageUrl] = useState(null);
-
   useEffect(() => {
-    const fetchImage = async () => {
-      try {
-        const response = await ApiService.get('/PedidosDoacao'); // Substitua '/image-endpoint' pelo endpoint correto
-        setImageUrl(response.imageUrl);
-      } catch (error) {
-        console.error('Erro ao buscar a imagem:', error.message);
-      }
-    };
-
-    fetchImage();
+    BuscarDoacoes();
   }, []);
 
+  const [doacoes, setDoacoes] = useState([]);
+  const [doacoesExibidas, setDoacoesExibidas] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
+  async function BuscarDoacoes() {
+    const response = await ApiService.get('/PedidosDoacao'); // Substitua '/image-endpoint' pelo endpoint correto
+    setDoacoes(response);
+    setDoacoesExibidas(response);
+    console.log(response);
+  }
+
+  function filtrarDoacoes(text) {
+    setSearchText(text);
+    if (text) {
+      const filteredDoacoes = doacoes.filter(doacao => 
+        doacao.titulo.toLowerCase().includes(text.toLowerCase()) ||
+        doacao.ong.nome.toLowerCase().includes(text.toLowerCase())
+      );
+      setDoacoesExibidas(filteredDoacoes);
+    } else {
+      setDoacoesExibidas(doacoes);
+    }
+  }
+  
+
   return (
-    <View style={styles.containerForm}>
-      <View style={styles.containerTitle}>
-        <Text style={styles.title}>Anuncios Publicados</Text>
-      </View>
-
-      {imageUrl ? (
-        <View style={styles.containerImage}>
-          <Image
-            source={{ uri: imageUrl }}
-            style={{ width: '100%', height: 200 }}
-            resizeMode="contain"
-          />
-        </View>
-      ) : (
-        <Text>Carregando imagem...</Text>
-      )}
-
+    <View style={styles.container}>
+        <TextInput
+        style={styles.searchBar}
+        placeholder="Pesquisar..."
+        value={searchText}
+        onChangeText={text => filtrarDoacoes(text)}
+      />
       <View>
-        <Text style={styles.lista}>Conserto de encanamento</Text>
+        <Text style={styles.textTitle}> Anúncios publicados </Text>
       </View>
-
-      <View>
-        <Text style={styles.lista2}>Publicado em 01/06/24</Text>
-      </View>
-
-      <View style={styles.containerImage}>
-        <Image
-          source={require('../../../assets/cesta_basica.png')}
-          style={{ width: '100%', height: 200 }}
-          resizeMode="contain"
-        />
-      </View>
-
-      <View>
-        <Text style={styles.lista}>Doação de alimento</Text>
-      </View>
-
-      <View>
-        <Text style={styles.lista2}>Publicado em 01/05/24</Text>
-      </View>
+      <ScrollView>
+        {doacoesExibidas?.map((doacao, index) => (
+          <CardDoacao key={index} doacao={doacao}></CardDoacao>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  containerTitle: {
+  container: {
     flex: 1,
-    backgroundColor: '#38a69d',
     padding: 10,
-    alignItems: 'center',
   },
-  title: {
-    fontSize: 24,
-    color: '#FFF',
-  },
-  lista: {
-    backgroundColor: '#38a69d',
-    padding: 10,
-    marginTop: 10,
-    color: '#FFF',
+  textTitle: {
     textAlign: 'center',
+    color: '#16CF8C',
+    fontWeight: '600',
+    fontSize: 25,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 3,
   },
-  lista2: {
-    backgroundColor: '#38a69d',
-    padding: 10,
-    marginTop: 5,
-    color: '#FFF',
-    textAlign: 'center',
-  },
-  containerImage: {
-    backgroundColor: '#38a69d',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  containerForm: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    paddingStart: '5%',
-    paddingEnd: '5%',
-    paddingTop: 20,
+  searchBar: {
+    height: 40,
+    borderColor: '#38a69d',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 10,
+    paddingHorizontal: 10,
   },
 });
--- DoaMaisMobile/src/Services/ApiService.js
+++ DoaMaisMobile/src/Services/ApiService.js
@@ -1,25 +1,11 @@
 import axios from "axios";
-import AuthService from "./AuthService";
-
-function createHeader() {
-    const jwt = AuthService.PegarToken();
-    if (jwt) {
-        return {
-            headers: {
-                Authorization: 'Bearer ' + jwt
-            }
-        }
-    }
-}
 
 const baseUrl = "https://localhost:7284/api";
 
 const ApiService = {
     async get(endpoint) {
-        const headers = createHeader();
-
         try {
-            const response = await axios.get(baseUrl + endpoint, headers);
+            const response = await axios.get(baseUrl + endpoint);
             return response.data;
         } catch (error) {
             throw new Error(`Erro ao realizar a requisição GET para ${endpoint}: ${error.message}`);
@@ -27,9 +13,8 @@ const ApiService = {
     },
 
     async post(endpoint, body) {
-        const header = createHeader();
         try {
-            const response = await axios.post(baseUrl + endpoint, body, header);
+            const response = await axios.post(baseUrl + endpoint, body);
             return response.data;
         } catch (error) {
             throw new Error(`Erro ao realizar a requisição POST para ${endpoint}: ${error.message}`);
